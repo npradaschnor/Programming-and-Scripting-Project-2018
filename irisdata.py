c430d4743bd58d20875b6f45e7ea46a059801067
npradaschnor <35732254+npradaschnor@users.noreply.github.com>
2018-03-26 14:05:03 +0100

    Update irisdata.py

--- irisdata.py
+++ irisdata.py
@@ -1,10 +1,43 @@
 #Noa P Prada Schnor 2018-03-24
 
+#import libraries to help to analyse the data set
+import csv
 import pandas
-# Pandas - library highlight: 1."Tools for reading and writing data between in-memory data structures and different formats: CSV and text files, Microsoft Excel, SQL databases, and the fast HDF5 format", 2."Columns can be inserted and deleted from data structures for size mutability;" https://pandas.pydata.org/; - As I want to delete one column from my csv file I decided to import /use pandas.
+import scipy
+import numpy
+import sklearn
+import seaborn as sns
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn import datasets
 
-#I named all the columns and then deleted the last column named 'class'. I've done that because the columns contined text(string) not numbers. That way, I can deal easier with numbers. The original file with data iris set is data/iris.csv and the new file created (without the last column) is data/datairis.csv
+with open ('data/iris.csv') as f: #open iris data set
+  names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class'] 
+  dataset = pandas.read_csv(f, names=names) #give each column a 'header' a 'name'
 
-data = pandas.read_csv('data/iris.csv') #select the file iris.csv
-data=data.drop(['class'],axis=1) #delete the column named 'class'
-data.to_csv('data/datairis.csv') #create new file that will not contain the column 'class'
+  print(dataset.shape) # get the info about row many rows and columns
+
+  # describe the data: count, mean, minimun/maximum values and percentiles.
+  print(dataset.describe())
+
+  dataset.hist('sepal-length') #plot the histogram of sepal lenght
+  plt.show() #show the histogram
+
+  dataset.hist('sepal-width')  # plot the histogram of sepal width
+  plt.show()  # show the histogram
+
+  dataset.hist('petal-length')  # plot the histogram of petal lenght
+  plt.show()  # show the histogram
+
+  dataset.hist('petal-width')  # plot the histogram of petal width
+  plt.show()  # show the histogram
