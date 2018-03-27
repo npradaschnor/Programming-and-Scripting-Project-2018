2b2ce0b7e4bf1d6aeebc844d81bde1835dd929e8
npradaschnor <35732254+npradaschnor@users.noreply.github.com>
2018-03-27 16:01:01 +0100

    Update irisdata.py

--- irisdata.py
+++ irisdata.py
@@ -2,7 +2,7 @@
 #Based on #https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
 #Based on https://campus.datacamp.com/courses/pandas-foundations/data-ingestion-inspection?ex=1
 
-#import libraries to help to analyse the data set
+#import key libraries to help to analyse the data set
 import csv
 import pandas
 import scipy
@@ -11,6 +11,7 @@ import sklearn
 import seaborn as sns
 from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
+plt.style.use('seaborn-bright') #style of plot = seaborn-bright (I can change based on what is available from print plt.style.available)
 from sklearn import model_selection
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
@@ -23,11 +24,13 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 from sklearn import datasets
 
+print(plt.style.available)  # list all available styles (matplot)
+
 with open ('data/iris.csv') as f: #open iris data set
   names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class'] 
   dataset = pandas.read_csv(f, names=names) #give each column a 'header' a 'name'
   
-#get knowing my dataset
+#get to knowing the dataset
   print(type(dataset)) #type
   print(dataset.shape) # get the info about row many rows and columns
   print(dataset.columns) #name of the columns
@@ -40,13 +43,33 @@ with open ('data/iris.csv') as f: #open iris data set
   print(dataset.describe())
 
   dataset.hist('sepal-length') #plot the histogram of sepal lenght
+  plt.savefig('iris_hist_sepallenght.png') #save plot
   plt.show() #show the histogram
 
   dataset.hist('sepal-width')  # plot the histogram of sepal width
+  plt.savefig('iris_hist_sepalwidth.png') #save plot
   plt.show()  # show the histogram
 
   dataset.hist('petal-length')  # plot the histogram of petal lenght
+  plt.savefig('iris_hist_petallenght.png') #save plot
   plt.show()  # show the histogram
 
   dataset.hist('petal-width')  # plot the histogram of petal width
+  plt.savefig('iris_hist_petalwidth.png') #save plot
   plt.show()  # show the histogram
+
+  sl_arr = dataset['sepal-length'].values
+  print(type(sl_arr)) #print the type of var
+
+  plt.plot(sl_arr) #plot array (matplotlib) sepal lenght
+  plt.savefig('iris_plotarray_sepallenght.png') #save plot
+  plt.show() # show the plot
+
+  dataset.plot() #plot dataframe (pandas)
+  plt.savefig('iris_plotdataframe.png') #save plot
+  plt.show() # show the plot
+
+  dataset.plot()
+  plt.yscale('log') #fixing scales - log scale on vertical axis
+  plt.savefig('iris.log_verticalaxis.png') #save plot
+  plt.show() # show the plot
